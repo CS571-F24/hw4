5e111785a2684a79f72b6970c4488bbad4417eb2
shreya <42049756+Shreyasuriya06@users.noreply.github.com>
2024-10-07 16:30:35 -0500

    Update Student.jsx

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,8 +1,18 @@
 const Student = (props) => {
-    return <div>
+    return (
+    <div className = "student-card">
         <h2>{props.name.first} {props.name.last}</h2>
-        {/* TODO Student data goes here! */}
-    </div>
+            <p><strong>Major(s):</strong> {props.major}</p>
+            <p><strong>{props.name.first} is taking {props.numCredits} credits.</strong></p>
+            <p><strong>From Wisconsin:</strong> {props.fromWisconsin ? "Yes" : "No"}</p>
+            <p><strong>They have {props.interests.length} interest(s) including:</strong></p>
+            <ul>
+                {props.interests.map((interest, index) => (
+                    <li key={index}>{interest}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default Student;
